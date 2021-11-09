dce9e6802b6ffda1dba6251fbed917aad6e19f07
vikto <vikto@DELL-E7450>
2021-11-09 12:30:51 +0200

    Добавьте файлы проекта.

---
+++ AP_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AP_2_Lib;
+
+namespace AP_2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+
+            bank.Name = (Console.ReadLine());
+            bank.Number_deposits = Convert.ToDouble(Console.ReadLine());
+            bank.Contribution = Convert.ToDouble(Console.ReadLine());
+            bank.Percent = Convert.ToDouble(Console.ReadLine());
+
+            bank.GenInfo();
+            Bank bank_2 = new Bank();
+
+            bank_2.Name = (Console.ReadLine());
+            bank_2.Number_deposits = Convert.ToDouble(Console.ReadLine());
+            bank_2.Contribution = Convert.ToDouble(Console.ReadLine());
+            bank_2.Percent = Convert.ToDouble(Console.ReadLine());
+
+            bank_2.GenInfo();
+
+
+        }
+    }
+}
---
+++ AP_2_Lib/Bank.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AP_2_Lib
+{
+    public class Bank
+    {
+        string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        double number_deposits;
+        public double Number_deposits { set { number_deposits = value; } }
+
+        double contribution;
+        public double Contribution { set { contribution = value; } }
+
+        double percent;
+        public double Percent { set { percent = value; } }
+
+        public double Sum
+        {
+            get { return number_deposits * contribution * (percent / 100); }
+        }
+
+        public void GenInfo()
+        {
+            Console.WriteLine($"Название банка : {name}\n" +
+                              $"Число кадров   : {number_deposits} \n" +
+                              $"Размер вкладов : {contribution} \n" +
+                              $"Процент        : {percent} % \n" + $"");
+            Console.WriteLine($"Общие выплаты по процентам = {Sum}");
+        }
+
+
+    }
+    c
+    }
+}
